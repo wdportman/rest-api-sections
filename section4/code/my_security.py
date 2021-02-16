d62357f445b450ccaec8d4be9c7928850c3b1180
Will <wdportman@gmail.com>
2021-02-16 15:29:09 -0800

    Create my_security and my_user files

---
+++ section4/code/my_security.py
@@ -0,0 +1,30 @@
+users = [
+  {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+  }
+]
+
+username_mapping = {'bob':   {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+  }
+}
+
+userid_mapping = { 1:   {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+  }
+}
+
+def authenticate(username, password):
+  user = username_mapping.get(username)
+  if user and user.password == password:
+    return user
+
+def identity(payload):
+  user_id = payload['identity']
+  return userid_mapping.get(user_id, None)
