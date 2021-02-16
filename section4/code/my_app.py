3907f98f004c910e4e057aa6f114e47cf1ce9413
Will <wdportman@gmail.com>
2021-02-15 21:21:25 -0800

    Add endpoints

--- section4/code/my_app.py
+++ section4/code/my_app.py
@@ -4,10 +4,25 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
   def get(self, name):
-    return {'student': name}
+    for item in items:
+      if item['name'] == name:
+        return item
+    return {'item': None}, 404
+  
+  def post(self, name):
+    item = {'name': name, 'price': 12.00}
+    items.append(item)
+    return item, 201
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+  def get(self):
+    return {'items': items}
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
