2489d4a8af4a5ad9306cdd2a7f22d8241762fe6e
Will <wdportman@gmail.com>
2021-02-16 21:56:46 -0800

    Finish section 5

--- section5/code/my_item.py
+++ section5/code/my_item.py
@@ -11,6 +11,38 @@ class Item(Resource):
 
   @jwt_required()
   def get(self, name):
+    item = self.find_by_name(name)
+    if item:
+      return item
+    return {'message': 'Item not found'}, 404
+
+  def post(self, name):
+    if Item.find_by_name(name):
+      return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+    data = Item.parser.parse_args()
+    item = {'name': name, 'price': data['price']}
+
+    try:
+      self.insert(item)
+    except:
+      return {"message": "An error occurred inserting the item"}, 500 #Internal Server Error
+
+    return item, 201
+
+  @classmethod
+  def insert(cls, item):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "INSERT INTO items VALUES (?, ?)"
+    cursor.execute(query, (item["name"], item["price"]))
+
+    connection.commit()
+    connection.close()
+
+  @classmethod
+  def find_by_name(cls, name):
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
@@ -22,32 +54,59 @@ class Item(Resource):
 
     if row:
       return {'item': {'name': row[0], 'price': row[1]}}
-    return {'message': 'Item not found'}, 404
-
-  
-  def post(self, name):
-    if next(filter(lambda x: x['name'] == name, items), None) is not None:
-      return {'message': "An item with name '{}' already exists.".format(name)}, 400
-    data = Item.parser.parse_args()
-    item = {'name': name, 'price': data['price']}
-    items.append(item)
-    return item, 201
 
   def delete(self, name):
-    global items
-    items = list(filter(lambda x: x['name'] != name, items))
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "DELETE FROM items WHERE name=?"
+    cursor.execute(query, (name,))
+
+    connection.commit()
+    connection.close()
     return {'message': 'Item deleted'}
 
   def put(self, name):
     data = Item.parser.parse_args()
-    item = next(filter(lambda x: x['name'] == name, items), None)
+
+    item = self.find_by_name(name)
+    updated_item = {'name': name, 'price': data['price']}
+
+    
     if item is None:
-      item = {'name': name, 'price': data['price']}
-      items.append(item)
+      try:
+        self.insert(updated_item)
+      except:
+        return {"message": "An error occurred inserting the item."}, 500
     else:
-      item.update(data)
-    return item
+      try:
+        self.update(updated_item)
+      except:
+        return {"message": "An error occurred updating the item."}, 500
+    return updated_item
+
+  @classmethod
+  def update(cls, item):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "UPDATE items SET price=? WHERE name=?"
+    cursor.execute(query, (item['price'], item['name']))
+
+    connection.commit()
+    connection.close()
+
 
 class ItemList(Resource):
     def get(self):
-        return {'items': items}
+      connection = sqlite3.connect('data.db')
+      cursor = connection.cursor()
+
+      query = "SELECT * FROM items"
+      result = cursor.execute(query)
+      items = []
+      for row in result:
+        items.append({'name': row[0], 'price': row[1]})
+
+      connection.close()
+      return {'items': items}
