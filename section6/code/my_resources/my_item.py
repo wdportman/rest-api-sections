ba81fc90874ed01d1134bd88bea1499b1f09891c
Will <wdportman@gmail.com>
2021-02-17 13:29:49 -0800

    Update item resource

--- section6/code/my_resources/my_item.py
+++ section6/code/my_resources/my_item.py
@@ -26,40 +26,30 @@ class Item(Resource):
     item = ItemModel(name, data['price'])
 
     try:
-      item.insert()
+      item.save_to_db()
     except:
       return {"message": "An error occurred inserting the item"}, 500 #Internal Server Error
 
     return item.json(), 201
 
   def delete(self, name):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = "DELETE FROM items WHERE name=?"
-    cursor.execute(query, (name,))
-
-    connection.commit()
-    connection.close()
-    return {'message': 'Item deleted'}
+    item = Item.find_by_name(name)
+    if item:
+      item.delete_from_db()
 
   def put(self, name):
     data = Item.parser.parse_args()
 
     item = ItemModel.find_by_name(name)
-    updated_item = ItemModel(name, data['price'])
     
     if item is None:
-      try:
-        updated_item.insert()
-      except:
-        return {"message": "An error occurred inserting the item."}, 500
+      item = ItemModel(name, data['price'])
     else:
-      try:
-        updated_item.update()
-      except:
-        return {"message": "An error occurred updating the item."}, 500
-    return updated_item.json()
+      item.price = data['price']
+    
+    item.save_to_db()
+
+    return item.json()
 
 
 class ItemList(Resource):
