0702d502410c1ee1bee33f2da14c1892e8306526
Will <wdportman@gmail.com>
2021-02-16 21:15:08 -0800

    Create items table in DB

--- section5/code/my_user.py
+++ section5/code/my_user.py
@@ -40,7 +40,6 @@ class User(object):
       return user
 
 class UserRegister(Resource):
-
     parser = reqparse.RequestParser()
     parser.add_argument('username',
       type=str,
@@ -52,17 +51,20 @@ class UserRegister(Resource):
       required=True,
       help="This field field cannot be left blank"
     )
+    
+    def post(self):
+      data = UserRegister.parser.parse_args()
 
-  def post(self):
-    data = UserRegister.parser.parse_args()
+      if User.find_by_username(data['username']):
+        return {"message": "A user with that username already exists"}, 400
+      
+      connection = sqlite3.connect('data.db')
+      cursor = connection.cursor()
+      
+      query = "INSERT INTO users VALUES (NULL, ?, ?)"
+      cursor.execute(query, (data['username'], data['password']))
+      
+      connection.commit()
+      connection.close()
 
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = "INSERT INTO users VALUES (NULL, ?, ?)"
-    cursor.execute(query, (data['username'], data['password']))
-
-    connection.commit()
-    connection.close()
-
-    return {"message": "User created successfully"}, 201
+      return {"message": "User created successfully"}, 201
