9fb4e8b63c440d2be37bc650ea2f7d044e8b87c3
Will <wdportman@gmail.com>
2021-02-17 13:45:54 -0800

    Small syntax fix

--- section6/code/my_models/my_user.py
+++ section6/code/my_models/my_user.py
@@ -9,7 +9,6 @@ class UserModel(db.Model):
   password = db.Column(db.String(80))
 
   def __init__(self, username, password):
-      self.id = _id
       self.username = username
       self.password = password
 
