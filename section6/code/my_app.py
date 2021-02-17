5fa9e1c7887523cdf336ceef68b20b82b17ae438
Will <wdportman@gmail.com>
2021-02-17 13:14:47 -0800

    Add SQLAlchemy to project

--- section6/code/my_app.py
+++ section6/code/my_app.py
@@ -7,6 +7,7 @@ from my_resources.my_user import UserRegister
 from my_resources.my_item import Item, ItemList
 
 app = Flask(__name__)
+app.config(['SQLALCHEMY_TRACK_MODIFICATIONS'] = False)
 app.secret_key = 'will'
 api = Api(app)
 
@@ -17,4 +18,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+  from my_db import db
+  db.init_app(app)
   app.run(port=5000, debug=True)
---
+++ section6/code/my_db.py
@@ -0,0 +1,3 @@
+from flask-sqlalchemy import sqlalchemy
+
+db = sqlalchemy()
