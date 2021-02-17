e93fbf3997bb706348c400f6ffcb5929ec41c48d
Will <wdportman@gmail.com>
2021-02-16 16:43:53 -0800

    Finish setting up reqparse

--- section4/code/my_app.py
+++ section4/code/my_app.py
@@ -13,6 +13,12 @@ jwt = JWT(app, authenticate, identity) #JWT creates a new endpoint: /auth
 items = []
 
 class Item(Resource):
+  parser = reqparse.RequestParser()
+  parser.add_argument('price',
+    type=float,
+    required=True,
+    help="This field cannot be left blank")
+
   @jwt_required()
   def get(self, name):
     item = next(filter(lambda x: x['name'] == name, items), None)
@@ -21,7 +27,7 @@ class Item(Resource):
   def post(self, name):
     if next(filter(lambda x: x['name'] == name, items), None) is not None:
       return {'message': "An item with name '{}' already exists.".format(name)}, 400
-    data = request.get_json()
+    data = Item.parser.parse_args()
     item = {'name': name, 'price': data['price']}
     items.append(item)
     return item, 201
@@ -32,12 +38,7 @@ class Item(Resource):
     return {'message': 'Item deleted'}
 
   def put(self, name):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-      type=float,
-      required=True,
-      help="This field cannot be left blank")
-    data = parser.parse_args()
+    data = Item.parser.parse_args()
     item = next(filter(lambda x: x['name'] == name, items), None)
     if item is None:
       item = {'name': name, 'price': data['price']}
